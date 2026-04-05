3d1688f86b14ee8dcb1ab291853c7e98b3476914
gouriparvathy-collab <gouriparvathy@ug.cusat.ac.in>
2026-04-05 18:45:18 +0530

    Create Count,sum,divisibleby7.c

---
+++ Count,sum,divisibleby7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i, count = 0, sum = 0;
+    for (i = 101; i < 200; i++) 
+    {
+        if (i % 7 == 0)
+         {
+            count++;
+            sum += i;
+         }
+    }
+    printf("Count = %d\n", count);
+    printf("Sum = %d\n", sum);
+    return 0;
+}
